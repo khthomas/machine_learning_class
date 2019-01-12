67e613d0b432439b0836195a645e0f8016a45db2
Unknown <khthomas@smu.edu>
2019-01-12 10:49:50 -0500

    Started to build function for custom search
    I know how to unpack the parameters, need to work on handling the data into test and train and k-fold cross validation. Then test it on some data and push results to github. Also share my approach with the class on slack.

--- DeathToGridSearch.py
+++ DeathToGridSearch.py
@@ -2,10 +2,32 @@ import numpy as np
 from sklearn.metrics import accuracy_score # other metrics?
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import KFold
+from sklearn.linear_model import LogisticRegression
+
 # adapt this code below to run your analysis
  
 # Due before live class 2
 # 1. Write a function to take a list or dictionary of clfs and hypers ie use logistic regression, each with 3 different sets of hyper parrameters for each
+
+# input is a list with format [model, hyperParamterdict], ideally the hyperparameters will be expanded to accept a list of inputs
+# right now it is just a single value to make sure variable unpacking is passed to the classifer correctly
+testInput = [
+[LogisticRegression, {'C': 1.0, 'solver': 'lbfgs', 'tol' : 1e-3}]
+]
+# testInput =  [LogisticRegression, {'C': [0.5, 1.0, 1.5], 'solver': ['lbfgs', 'liblinear'], 'tol' : [1e-4, 1e-3, 1e-2]}]
+def DeathToGridSearch(data, searchList):
+  M, L, n_folds = data
+
+  for searchVar in searchList:
+    clf = searchVar[0]
+    clf = clf(**searchVar[1])
+    clf.fit(M,L)
+    
+
+
+
+
+
  
 #Due before live class 3
 # 2. expand to include larger number of classifiers and hyperparmater settings
@@ -41,3 +63,12 @@ def run(a_clf, data, clf_hyper={}):
   return ret
  
 results = run(RandomForestClassifier, data, clf_hyper={})
+
+
+#testing area
+clfTest = [LogisticRegression, {'C': 1.0, 'solver': 'lbfgs', 'tol' : 1e-3}]
+
+clf = clfTest[0]
+clf = clf(**clfTest[1])
+clf.fit
+  
