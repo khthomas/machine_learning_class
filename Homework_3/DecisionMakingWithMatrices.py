86f8768a6cc0db4aee147975a482e03c461093f7
Unknown <khthomas@smu.edu>
2019-02-18 22:36:13 -0500

    Made matrices, analysis can begin

--- Homework_3/DecisionMakingWithMatrices.py
+++ Homework_3/DecisionMakingWithMatrices.py
@@ -1,5 +1,4 @@
 # Decision making with Matrices
-
 # This is a pretty simple assingment.  You will do something you do everyday, but today it will be with matrix manipulations.
 
 # The problem is: you and your work firends are trying to decide where to go for lunch. You have to pick a resturant thats best for everyone.  Then you should decided if you should split into two groups so eveyone is happier.
@@ -8,7 +7,7 @@
 
 # This process was thoughly investigated in the operation research community.  This approah can prove helpful on any number of decsion making problems that are currently not leveraging machine learning.
 
-
+import numpy as np
 
 # You asked your 10 work friends to answer a survey. They gave you back the following dictionary object.
 people = {'Jane': {'willingness to travel': 1,
@@ -21,6 +20,25 @@ people = {'Jane': {'willingness to travel': 1,
                   },
 
           }
+
+people = {}
+
+names  = ['Kyle', 'Thomas', 'George', 'Max', 'Mary', 'Martha', 'Tacitus', 'Emily', 'Ruth', 'Jane']
+cats = ['Close Distance', 'Desire for New Experience', 'Cost', 'American', 'Asian', 'Italian', 'Indian', 'Mexican', 'Hipster Points', 'Vegetarian']
+
+def make_people_dict(names, categories):
+    people = {}
+
+    for n in names:
+        random_prefs = np.random.randint(0, 11, len(categories))
+        dict_input = dict(zip(categories, random_prefs))
+        people[n] = dict_input
+    
+    return people
+
+people = make_people_dict(names, cats)
+
+
 
 # Transform the user data into a matrix(M_people). Keep track of column and row ids.
 
@@ -37,11 +55,21 @@ resturants  = {'flacos':{'distance' :
 
 }
 
+rests = ['Big Johns Pizza', 'Dames Chicken and Waffels', 'Pho King', 'Sandwhich Stop', 'Valu Buffet', 'Burger Fi', 'Fish Market', 'Curry Stop']
+
+resturants = make_people_dict(rests, cats)
+
+##############################################
+### QUESTIONS 
+##############################################
+
 
 # Transform the restaurant data into a matrix(M_resturants) use the same column index.
+M_resturants = np.array([list(resturants[r].values()) for r in rests])
+M_people = np.array([list(people[p].values()) for p in people])
+
 
 # The most imporant idea in this project is the idea of a linear combination.
-
 # Informally describe what a linear combination is and how it will relate to our resturant matrix.
 
 # Choose a person and compute(using a linear combination) the top restaurant for them.  What does each entry in the resulting vector represent.
